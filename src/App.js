6bd41733209579402a1a235827df61a424eac4f5
Charlie <charles.j.montoya@gmail.com>
2021-04-22 13:07:48 -0400

    Added Navbar and Auth Buttons App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import Nav from './components/navigation_components/Nav';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+    
+      <Nav 
+          setOpenSignUp={setOpenSignUp}
+          setOpenSignIn={setOpenSignIn}
+          setUser={setUser}
+      />
+
     </div>
   );
 }
