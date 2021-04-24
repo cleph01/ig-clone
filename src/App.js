4e9653c818853990c69017f2364982ceed258841
Charlie <charles.j.montoya@gmail.com>
2021-04-24 11:13:11 -0400

    Erased App to copy IG clone video; added seed.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-
-import Nav from './components/navigation_components/Nav';
-
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-    
-      <Nav 
-          setOpenSignUp={setOpenSignUp}
-          setOpenSignIn={setOpenSignIn}
-          setUser={setUser}
-      />
-
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
