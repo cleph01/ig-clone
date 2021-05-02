dafb73ed0229c8286dcc17b6da5a8d975eefcd52
Charlie <charles.j.montoya@gmail.com>
2021-05-02 17:09:31 -0400

    Made Login Form background white

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,37 +22,45 @@ function Login() {
         <img src="/images/iphone-with-profile.jpeg" alt="iPhone with Instagram" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-s w-6/12 mb-4" />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-s w-6/12 mb-4" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email Address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+          <form method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email Address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
             border-gray-primary rounded mb-2"
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
             border-gray-primary rounded mb-2"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <button
-            disabled={isInvalid}
-            type="submit"
-            className={`bg-blue-500 text-white w-full rounded h-8 font-bold 
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold 
           ${isInvalid && 'opacity-50'}`}
-          >
-            Login
-          </button>
-        </form>
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div
+          className="flex justify-center items-center flex-col w-full bg-white p-4 border 
+      border-gray-primary"
+        >
+          <p className="test-sm">Don't have an account?</p>
+        </div>
       </div>
     </div>
   );
